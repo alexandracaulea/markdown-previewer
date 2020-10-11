286ee536682b18a6d9f6dfd6a71d81241cd050b9
Alexandra Caulea <alexandra.caulea@gmail.com>
2020-10-11 21:08:38 +0300

    Sanitize the data in order to prevent XSS atack.

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import DOMPurify from 'dompurify';
 import marked from 'marked';
 import './Preview.css';
 
 export default function Preview({ textareaValue }) {
+  const clean = DOMPurify.sanitize(textareaValue);
   return (
     <div
       id="preview"
       className="Preview"
       dangerouslySetInnerHTML={{
-        __html: marked(textareaValue, { breaks: true }),
+        __html: marked(clean, { breaks: true }),
       }}
     />
   );
